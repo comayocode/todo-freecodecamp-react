241d2240b69aa242a14f8d448ba7dec26d9d86f3
Comayocode <ronalc726@gmail.com>
2023-10-12 12:12:03 -0500

    Añadir eliminar, completar, agregar y enviar tarea

--- src/components/TareaFormulario.jsx
+++ src/components/TareaFormulario.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import '../stylesheets/TareaFormulario.css'
+import React, { useState } from 'react';
+import '../stylesheets/TareaFormulario.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function TareaFormulario(props) {
+  const [input, setInput] = useState('');
+
+  const manejarCambio = (e) => {
+    setInput(e.target.value);
+  };
+
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completado: false,
+    };
+    props.onSubmit(tareaNueva);
+  };
+
   return (
-    <form className='tarea-formulario'>
+    <form onSubmit={manejarEnvio} className='tarea-formulario'>
       <input
         className='tarea-input'
         type='text'
         placeholder='Escriba la tarea'
         name='texto'
+        onChange={manejarCambio}
       />
       <button className='tarea-boton'>Agregar Tarea</button>
     </form>
